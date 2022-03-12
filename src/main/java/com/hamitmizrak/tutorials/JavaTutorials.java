b278c598085f9bbab323fa9673352eb39eea7e85
hamitmizrak <hamitmizrak@gmail.com>
2022-03-12 21:13:09 +0300

    Hatırlatma

---
+++ src/main/java/com/hamitmizrak/tutorials/JavaTutorials.java
@@ -0,0 +1,67 @@
+package com.hamitmizrak.tutorials;
+
+import java.util.Arrays;
+
+public class JavaTutorials {
+
+    public static void main(String[] args) {
+//        Arrays.sort();
+//        this.
+//        super
+//        break continue return
+//        optional
+        //cast
+        //primitive type
+        //wrapper class
+        //Random
+        //Math
+        // %s %d %f ==> printf()
+        //final
+        //static
+        //decoder encoder
+        //instanceof
+        //public private protected
+        //String
+        //StringBuilder
+        //StringTokenizer
+        //StringBuffer
+        //Enum
+        //Multithread
+
+        //S.O.L.I.D
+        //KISS
+        //DRY
+        //MONAD
+        //List Set Map
+        //Liste:LinkedList ArrayList
+        //Generic <>
+        //Iterator
+        //OOP
+        //interface
+        //abstract new
+        //inheritance
+
+        //UML - + __
+        //class
+        //POJO
+        //Bean
+        //Serileştirme
+        //IO
+        //JAR WAR
+        //Dağıtık
+        //server
+
+        //constructor
+        //excepiton
+        //error
+
+        //Java 8 : stream,map,filter,sort
+        //Optional
+        //Lambda Expression
+        //Method Referances ::
+
+        //D.P.
+
+
+    }
+}
